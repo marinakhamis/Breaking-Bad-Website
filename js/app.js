53354b5cf1bad9fbc7104330678fd0ddcb8ee86c
Marina khamis <35566244+marinakhamis@users.noreply.github.com>
2022-01-29 02:36:09 +0200

    Adding Back to top btn

--- js/app.js
+++ js/app.js
@@ -49,3 +49,27 @@ fetch("https://www.breakingbadapi.com/api/episodes")
         });
         return seasonsArr;
     })
+
+
+
+
+// To top button
+//Get the button
+var mybutton = document.getElementById("to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
